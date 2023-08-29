66f9aa052d62f1e1e5ba11c4309164820d440da6
hubi <hubcio1765@gmail.com>
2023-08-29 12:17:57 +0200

    Bug fix

--- ASCII_art.py
+++ ASCII_art.py
@@ -4,8 +4,7 @@ import os
 import cv2
 
 
-
-ASCII_CHARS = "@B%8WM#*oahkbdpwmZO0QCJYXzcvnxrjft/\|()1{}[]-_+~<>i!lI;:,"
+ASCII_CHARS = "@B%8WM#*oahkbdpwmZO0QCJYXzcvnxrjft/\|()1{}[]-_+~<>i!lI;:, "
 
 
 # Image resizing
@@ -19,6 +18,7 @@ def resize(image, new_width=412):
 # Conversion to grayscale
 def grayify(image):
     return image.convert("L")
+    
 
 # Mapping pixels to ASCII characters
 def pixels_to_ascii(image):
@@ -31,13 +31,15 @@ def pixels_to_ascii(image):
 def convert_to_ascii(image, new_width=412):
     new_image_data = pixels_to_ascii(grayify(resize(image)))
     pixel_count = len(new_image_data)
-    return "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+    result = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+    return result
 
 # Checking if the path to the image is correct
 def convert_img_to_ascii(image_path):
     try:
-        image = Image.open(image_path)   
-        return convert_to_ascii(image)  
+        image = Image.open(image_path) 
+        image = convert_to_ascii(image)
+        print(image)
     except: 
         return "Invalid path"
 
@@ -45,7 +47,6 @@ def play_ascii_animation(frames, delay=0.1):
     for frame in frames:
         print(frame)
         time.sleep(delay)   
-        os.system('cls' if os.name == 'nt' else 'clear')
 
 # Creating video in ascii
 def generate_ascii_video(video_path):
@@ -60,13 +61,15 @@ def generate_ascii_video(video_path):
         pil_frame = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         ascii_frame = convert_to_ascii(pil_frame, new_width=412)
         ascii_frames.append(ascii_frame)
-        play_ascii_animation(ascii_frames) 
+
+    play_ascii_animation(ascii_frames) 
     cap.release()
 
 
 print("1: Image to Ascii \n2: Video to Ascii")
 choice = input("Enter your choice: ")
 path = input("\nEnter the file: ")
+
 if choice == '1':
     convert_img_to_ascii(path)
 elif choice == '2': 
